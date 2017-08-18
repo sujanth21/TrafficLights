16a08f0ae5ca6af170272c80963566097140d1ec
Sujanth <sujanth1987@gmail.com>
2017-08-18 22:00:38 +1000

    Added variables and actions

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trafficImage: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var startTimer = Timer()
+    var scoreTimer = Timer()
+    
+    var timeInt = 0
+    var scoreInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func startGame(_ sender: Any) {
     }
-
-
+    
 }
 
